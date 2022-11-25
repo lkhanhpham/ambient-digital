5bf1f7317428d4137441aa333658a63b8ef3e9ef
estolz <estolz@smail.uni-koeln.de>
2022-11-25 21:51:39 +0100

    field interface should work now

--- Backend/quiz/serializers.py
+++ Backend/quiz/serializers.py
@@ -16,6 +16,8 @@ class QuestionSerializer(serializers.ModelSerializer):
         model = Question
         fields = ('question_text','answer_text' ,'pub_date','author')
 
+#This class will lead to merge-conflicts, when merging the feature/backend-Fragen-erweitern branch into a branch containing this class
+#When this happens replace this classs with the FieldSerializer in the other branch, because the model has been updated
 class FieldSerializer(serializers.ModelSerializer):
     categorie_name= serializers.ReadOnlyField(source='categorie.categorie_name')
     question_text= serializers.ReadOnlyField(source='question.question_text')
@@ -23,6 +25,25 @@ class FieldSerializer(serializers.ModelSerializer):
     class Meta:
         model = Field
         fields = ('point','question','question_text','categorie','categorie_name','quiz',)
+
+    def create(self, validated_data: dict):    
+
+        created_field = Field.objects.create(
+                point=validated_data.get('point'),
+                categorie=validated_data.get('categorie'),
+                question=validated_data.get('question'),
+                quiz=validated_data.get('quiz')
+            )
+        return created_field
+    
+    def update(self, instance, validated_data):
+        instance.point = validated_data.get('point', instance.point)
+        instance.categorie = validated_data.get('categorie', instance.categorie)
+        instance.quiz = validated_data.get('quiz', instance.quiz)
+        instance.question = validated_data.get('question',instance.question)
+
+        instance.save()
+        return instance
 
 class WholeQuizSerializer(serializers.ModelSerializer):
     quiz_field=FieldSerializer(many=True, read_only=True)
