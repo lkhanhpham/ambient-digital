fff3cb671c14abab60c27be869b2fb47bfc2809b
tdiepenb <tdiepenb@smail.uni-koeln.de>
2022-11-30 16:27:21 +0100

    added a baseURL to playwright.config.js and gitlab-ci.yml. also adjusted the reporter in playwright.config.js

--- Frontend/tests/flow.spec.ts
+++ Frontend/tests/flow.spec.ts
@@ -1,11 +1,11 @@
 import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
-  await page.goto('localhost:3000/');
+  await page.goto('/');
   
   await page.locator('.nav__menu-bar').click();
   await page.getByRole('link', { name: 'My Library' }).click();
 
-  await expect(page).toHaveURL('localhost:3000/Library');
+  await expect(page).toHaveURL('/Library/');
   
 });
