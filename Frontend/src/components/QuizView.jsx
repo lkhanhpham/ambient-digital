388849ce72fa14cf595f99c7d327f0b3019887c6
Alisa Lied <alied@smail.uni-koeln.de>
2022-12-01 08:48:24 +0100

    quiz delete function and edit

--- Frontend/src/components/QuizView.jsx
+++ Frontend/src/components/QuizView.jsx
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import Quiz from './quizCard'
-import { Link} from "react-router-dom";
+import { Link, useNavigate} from "react-router-dom";
+import axios from "axios"
 //all created quizes are inserted into the quizview
 const QuizView = () => {
     const [quizzes, setQuizzes] = useState([])
+    const navigate = useNavigate()
 
     const getAllQuizzes = async () => {
         const response = await fetch('http://127.0.0.1:8000/api/wholequiz/')
@@ -26,12 +28,35 @@ const QuizView = () => {
 
     const deleteItem = async (quizId) => {
         // delete function
+        console.log(quizId)
+        if (window.confirm('Do you really want to delete this question?')){
+            axios(
+                {
+                    method: "DELETE",
+                    url: "http://localhost:8000/api/quiz/"+quizId+"/",
+                    headers: {'Content-Type': 'application/json'}
+                }
+            ).then((response) => {
+                console.log(response.data)
+            })
+
+            window.location.reload();
+        }else{
+            // They clicked no
+        }
     }
 
     const editItem = async (quizId) => {
         // edit function
         //leads uder to the quizEditor page for the chosen quiz
-
+        console.log(quizId)
+        navigate("/QuizCreator/"+quizId+"/", 
+            {state: 
+            {   
+                id: quizId, 
+            }
+        } 
+        )
     }
 
     return (
