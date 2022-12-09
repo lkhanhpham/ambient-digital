b734318b6a96c72f77697ee2bc0a9ec137d6b526
tdiepenb <tdiepenb@smail.uni-koeln.de>
2022-12-09 22:10:48 +0100

    reduced test to a basic test

--- Frontend/tests/flowtest.spec.ts
+++ Frontend/tests/flowtest.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('test_registration_login_questioncreation_questiondeletion', async ({ page }) => {
+test('test_not_logged_in_navigation', async ({ page }) => {
 
   await page.goto('/');
 
@@ -9,63 +9,7 @@ test('test_registration_login_questioncreation_questiondeletion', async ({ page 
   await page.getByRole('link', { name: 'Registration' }).click();
   await expect(page).toHaveURL('/Registration');
 
-  await page.getByPlaceholder('Username').click();
-
-  await page.getByPlaceholder('Username').fill('test1234');
-
-  await page.getByPlaceholder('Username').press('Tab');
-
-  await page.getByPlaceholder('E-Mail').fill('test1234@exm.pl');
-
-  await page.getByPlaceholder('E-Mail').press('Tab');
-
-  await page.locator('#password').fill('test1234test1234');
-
-  await page.locator('#password').press('Tab');
-
-  await page.getByPlaceholder('Confirm Password').fill('test1234test1234');
-
-  await page.getByRole('button', { name: 'Submit' }).click();
-
-  await expect(page).toHaveURL('/login');
-
-  await page.getByPlaceholder('Enter Username').click();
-
-  await page.getByPlaceholder('Enter Username').fill('test1234');
-
-  await page.getByPlaceholder('Enter Username').press('Tab');
-
-  await page.getByPlaceholder('Enter Password').fill('test1234test1234');
-
-  await page.getByRole('button', { name: 'Login' }).click();
-  await expect(page).toHaveURL('/Library');
-
-  await page.locator('.nav__menu-bar').click();
-
-  await page.getByRole('button', { name: 'Create question' }).click();
-  await expect(page).toHaveURL('/QuestionCreator/SC');
-
-  await page.getByPlaceholder('New Question').click();
-
-  await page.getByPlaceholder('New Question').fill('TestQuestion1');
-
-  await page.getByPlaceholder('New Question').press('Tab');
-
-  await page.getByPlaceholder('New Answer').fill('TestAnswer1');
-
-  await page.getByRole('button', { name: 'Create' }).click();
-
-  await page.getByRole('button', { name: 'Close and back to overview.' }).click();
-  await expect(page).toHaveURL('/Library');
-
-  await page.getByRole('button', { name: 'Delete' }).click();
-
-  await page.getByRole('button', { name: 'Yes!' }).click();
-  await expect(page).toHaveURL('/Library');
-
-  await page.locator('.nav__menu-bar').click();
-
-  await page.getByRole('link', { name: 'Logout' }).click();
-  await expect(page).toHaveURL('/');
-
+  await page.getByRole('link', { name: 'Login' }).click();
+  await expect(page).toHaveURL('/Login');
+  
 });
