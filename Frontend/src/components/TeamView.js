6a2ba64ffa54b427c0f2b8993e2e7b530837f523
Linh Khanh <lpham1@smail.uni-koeln.de>
2022-12-22 17:48:11 +0100

    create component to show teams

---
+++ Frontend/src/components/TeamView.js
@@ -0,0 +1,12 @@
+const TeamView = () => {
+  return (
+    <>
+      <div className="container"></div>
+      <div className="text-dark d-flex justify-content-center align-self-center pt-5 pb-3">
+        <h3 className="big-title">Teams</h3>
+      </div>
+    </>
+  );
+};
+
+export default TeamView;
