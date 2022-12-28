24d6052dd7c196613df0886f66fae9898313146b
cbous1 <cbous1@smail.uni-koeln.de>
2022-12-28 21:45:48 +0100

    fixed old test and added more tests

--- Frontend/tests/flowtest.spec.ts
+++ Frontend/tests/flowtest.spec.ts
@@ -9,6 +9,7 @@ test('test_not_logged_in_navigation', async ({ page }) => {
   await page.getByRole('link', { name: 'Registration' }).click();
   await expect(page).toHaveURL('/Registration');
 
+  await page.locator('.nav__menu-bar').click();
   await page.getByRole('link', { name: 'Login' }).click();
   await expect(page).toHaveURL('/Login');
   
---
+++ Frontend/tests/test-1.spec.ts
@@ -0,0 +1,126 @@
+import { test, expect } from '@playwright/test';
+
+test('Registration_Create_Question', async ({ page }) => {
+
+  await page.goto('/');
+
+  await page.locator('div:has-text("Not Logged InHomeLoginRegistration")').nth(2).click();
+
+  await page.locator('.nav__menu-bar').click();
+
+  await page.getByRole('link', { name: 'Registration' }).click();
+  await expect(page).toHaveURL('http://localhost:3000/Registration');
+
+  await page.getByPlaceholder('Username').click();
+
+  await page.getByPlaceholder('Username').fill('Test12345678');
+
+  await page.getByPlaceholder('Username').press('Tab');
+
+  await page.getByPlaceholder('E-Mail').fill('Test12345678@ex.com');
+
+  await page.getByPlaceholder('E-Mail').press('Tab');
+
+  await page.locator('#password').fill('Test123456789Test12345678');
+
+  await page.locator('#password').press('Tab');
+
+  await page.getByPlaceholder('Confirm Password').fill('Test123456789Test12345678');
+
+  await page.getByRole('button', { name: 'Submit' }).click();
+
+  await(10)
+
+  await expect(page).toHaveURL('http://localhost:3000/login');
+
+  await page.getByPlaceholder('Enter Username').click();
+
+  await page.getByPlaceholder('Enter Username').fill('Test12345678');
+
+  await page.getByPlaceholder('Enter Username').press('Tab');
+
+  await page.getByPlaceholder('Enter Password').fill('Test123456789Test12345678');
+
+  await page.getByRole('button', { name: 'Login' }).click();
+  await expect(page).toHaveURL('http://localhost:3000/Library');
+
+  await page.locator('.nav__menu-bar > div:nth-child(2)').click();
+
+  await page.getByRole('link', { name: 'Home' }).click();
+  await expect(page).toHaveURL('http://localhost:3000/');
+
+  await page.locator('.nav__menu-bar').click();
+
+  await page.getByRole('link', { name: 'My Library' }).click();
+  await expect(page).toHaveURL('http://localhost:3000/Library');
+
+  await page.locator('.nav__menu-bar > div:nth-child(2)').click();
+
+  await page.getByRole('link', { name: 'Quiz-Room' }).click();
+  await expect(page).toHaveURL('http://localhost:3000/');
+
+  await page.locator('.nav__menu-bar').click();
+
+  await page.getByRole('link', { name: 'Create Quiz' }).click();
+  await expect(page).toHaveURL('http://localhost:3000/QuizCreator');
+
+  await page.locator('.nav__menu-bar > div:nth-child(2)').click();
+
+  await page.getByRole('link', { name: 'Create Question' }).click();
+  await expect(page).toHaveURL('http://localhost:3000/QuestionCreator/SC');
+
+  await page.locator('#root div:has-text("HomeMy LibraryQuiz-RoomCreate QuizCreate QuestionCreate CategoriesLogout")').nth(3).click();
+
+  await page.getByRole('link', { name: 'Create Categories' }).click();
+  await expect(page).toHaveURL('http://localhost:3000/CategoryCreator');
+
+  await page.getByPlaceholder('New Category').click();
+
+  await page.getByPlaceholder('New Category').fill('Cat1');
+
+  await page.getByRole('button', { name: 'Create' }).click();
+
+  await page.getByRole('button', { name: 'Continue' }).click();
+
+  await page.locator('.nav__menu-bar').click();
+
+  await page.getByRole('link', { name: 'Create Question' }).click();
+  await expect(page).toHaveURL('http://localhost:3000/QuestionCreator/SC');
+
+  await page.getByPlaceholder('New Question').click();
+
+  await page.getByPlaceholder('New Question').fill('TestFrage1');
+
+  await page.getByPlaceholder('New Answer').click();
+
+  await page.getByPlaceholder('New Answer').fill('TestAntwort1');
+
+  await page.getByRole('button', { name: 'Create' }).click();
+
+  await page.getByRole('button', { name: 'Close and back to overview.' }).click();
+  await expect(page).toHaveURL('http://localhost:3000/Library');
+
+  await page.getByRole('button', { name: 'Delete' }).click();
+
+  await page.getByRole('button', { name: 'Yes!' }).click();
+  await expect(page).toHaveURL('http://localhost:3000/Library');
+
+  await page.locator('.nav__menu-bar').click();
+
+  await page.getByRole('link', { name: 'Logout' }).click();
+  await expect(page).toHaveURL('http://localhost:3000/');
+
+  await page.getByRole('link', { name: 'Home' }).click();
+  await expect(page).toHaveURL('http://localhost:3000/');
+
+  await page.locator('.nav__menu-bar > div:nth-child(2)').click();
+
+  await page.getByRole('link', { name: 'Login' }).click();
+  await expect(page).toHaveURL('http://localhost:3000/Login');
+
+  await page.locator('.nav__menu-bar').click();
+
+  await page.getByRole('link', { name: 'Registration' }).click();
+  await expect(page).toHaveURL('http://localhost:3000/Registration');
+
+});
